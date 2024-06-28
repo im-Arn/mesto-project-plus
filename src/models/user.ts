13fe7d31aa2ba2bd80e904aa5c74ef8bb8229f18
ARN <this.is.arn@yandex.ru>
2024-06-29 00:37:48 +0400

    Backend.Sprint2 Rev.1 add user validation, model method

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,28 +1,54 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema, Model, Document } from 'mongoose';
+import validator from 'validator';
+import { defaultProfile } from '../constants/const';
 
-export type TUser = {
+interface IUser {
   name: string;
   about: string;
   avatar: string;
+  email: string;
+  password: string;
 };
 
-const userSchema = new Schema<TUser>({
+interface UserModel extends Model<IUser> {
+  findUserByCredentials: (
+    email: string,
+    password: string
+  ) => Promise<Document<unknown, any, IUser>>;
+}
+
+const userSchema = new Schema<IUser, UserModel>({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    // required: true,
+    default: defaultProfile.name,
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 200,
-    required: true,
+    // required: true,
+    default: defaultProfile.about,
   },
   avatar: {
     type: String,
+    // required: true,
+    default: defaultProfile.avatar,
+    validate: (v: string) => validator.isURL(v),
+  },
+  email: {
+    type: String,
     required: true,
+    unique: true,
+    validate: (v: string) => validator.isEmail(v),
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
 
-export default model<TUser>('user', userSchema);
+export default model<IUser>('user', userSchema);
